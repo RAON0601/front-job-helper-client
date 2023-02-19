cf3e966e83c8e7b8c8e6b33595d451a6a9dff83f
김정민 <68500898+RAON0601@users.noreply.github.com>
2023-02-19 15:00:49 +0900

    배포 관련 파일 추가

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const http = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://ec2-13-209-16-98.ap-northeast-2.compute.amazonaws.com:5000/api',
   withCredentials: true,
 });
